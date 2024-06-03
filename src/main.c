fb613b350983ef85feb8e0df12999c6ca739fb91
Tech-h <alerma0904@gmail.com>
2024-06-03 10:45:12 -0700

    Can now create a chunk, write an instruction to it, and then extract that instrcution back out. Basically encoding and decoding of bytecode. Also set up chunk dynamic memory allocation and then deallocation.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-int main()
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
+
+int main(int argc, const char* argv[])
 {
-	printf("Hello world");
-	printf("Yoooo!");
+	Chunk chunk;
+	initChunk(&chunk);
+	writeChunk(&chunk, OP_RETURN);
+
+	disassembleChunk(&chunk, "test chunk");
+	freeChunk(&chunk);
 	return 0;
 }
